97838a6c715dcd09cdff3c1bc60a55ba5fbb2f45
chan <chan.jor.zz@gmail.com>
2022-11-15 12:49:43 +0900

    add select option, jquery, vanillaJS

--- app.py
+++ app.py
@@ -21,5 +21,45 @@ def recommend():
 def reviews():
     return render_template('review.html')
 
+@app.route("/recommend/post/list", methods=["POST"])
+def recomment_post():
+    name_receive = request.form['name_give']
+    choose_receive = request.form['choose_give']
+    url_receive = request.form['url_give']
+    comment_receive = request.form['comment_give']
+
+    doc = {
+        'name':name_receive,
+        'choose':choose_receive,
+        'url':url_receive,
+        'comment':comment_receive
+    }
+
+    db.recommend.insert_one(doc)
+    return jsonify({'msg': '등록 완료!'})
+
+@app.route("/recommend/post/list", methods=["GET"])
+def recommend_list():
+    recommend_list = list(db.recommend.find({}, {'_id':False}))
+    return jsonify({'recommends':recommend_list})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
+
+# @app.route("/bucket/done", methods=["POST"])
+# def bucket_done():
+#     num_receive = request.form['num_give']
+#
+#     db.bucketss.update_one({'num':int(num_receive)}, {'$set': {'done':1}})
+#
+#     return jsonify({'msg': '버킷 완료!'})
+
+# @app.route("/bucket/cancle", methods=["POST"])
+# def bucket_cancle():
+#     num_receive = request.form['num_give']
+#
+#     db.bucketss.update_one({'num':int(num_receive)}, {'$set': {'done':0}})
+#
+#     return jsonify({'msg': '취소 완료!'})
+
