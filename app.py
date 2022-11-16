1b64126ebdfb18ece33f44f7b6ffac6030c7612f
rgngr <wlals8369@naver.com>
2022-11-16 15:25:12 +0900

    last

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ import certifi
 ca = certifi.where()
 
 client = MongoClient('mongodb+srv://rgngr:rgngr@cluster0.apj6ogn.mongodb.net/cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
-db = client.dbsparta
+db = client.hanghae99_08
 
-@app.route('/')
+@app.route('/course')
 def home():
     return render_template('course.html')
 
-@app.route("/post", methods=["POST"])
-def post():
+@app.route("/course/post", methods=["POST"])
+def post_course():
     url_receive = request.form['url_give']
     desc_receive = request.form['desc_give']
     location_receive = request.form['location_give']
@@ -28,10 +28,10 @@ def post():
 
     return jsonify({'msg':'저장 완료!'})
 
-@app.route("/list", methods=["GET"])
-def show():
+@app.route("/course/list", methods=["GET"])
+def show_course():
     course_list = list(db.course.find({}, {'_id': False}))
     return jsonify({'courses':course_list})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5353, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
