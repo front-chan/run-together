31b1ce1d3e774ce78f06c0fd612fe7d34cff1037
chan <chan.jor.zz@gmail.com>
2022-11-16 15:33:35 +0900

    mongodb

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import random
 
 from pymongo import MongoClient
 import certifi
-
 ca = certifi.where()
 client = MongoClient('mongodb+srv://rgngr:rgngr@cluster0.apj6ogn.mongodb.net/cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.hanghae99_08
