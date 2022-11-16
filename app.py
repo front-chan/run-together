e36183064494629f5cb3a510bfb34bff2049ba3b
rgngr <wlals8369@naver.com>
2022-11-16 12:55:49 +0900

    new

--- app.py
+++ app.py
@@ -1,49 +1,37 @@
-from pymongo import MongoClient
-import datetime
-from flask import Flask, render_template, jsonify, request
-from datetime import datetime
-
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
+
+from pymongo import MongoClient
+import certifi
+
+ca = certifi.where()
+
 client = MongoClient('mongodb+srv://rgngr:rgngr@cluster0.apj6ogn.mongodb.net/cluster0?retryWrites=true&w=majority')
-db = client.hanghae99_100108
+db = client.dbsparta
 
 @app.route('/')
 def home():
     return render_template('course.html')
 
-@app.route('/course', methods=['GET'])
-def show_course():
-    courses = list(db.course.find({}, {'_id': False}))
-    return jsonify({'all_course': courses})
-
-@app.route('/course/post', methods=['POST'])
-def post_course():
-    location_receive = request.form['location_give']
+@app.route("/post", methods=["POST"])
+def post():
+    url_receive = request.form['url_give']
     desc_receive = request.form['desc_give']
-
-    file = request.files["file_give"]
-
-    extension = file.filename.split('.')[-1]
-
-    today = datetime.now()
-    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
-
-    filename = f'file-{mytime}'
-
-    save_to = f'static/{filename}.{extension}'
-    file.save(save_to)
+    location_receive = request.form['location_give']
 
     doc = {
-        'location':location_receive,
+        'url':url_receive,
         'desc':desc_receive,
-        'file': f'{filename}.{extension}',
-        'time': today.strftime('%Y.%m.%d')
+        'location':location_receive
     }
-
     db.course.insert_one(doc)
 
-    return jsonify({'msg': '저장 완료!'})
+    return jsonify({'msg':'저장 완료!'})
 
+@app.route("/list", methods=["GET"])
+def list():
+    course_list = list(db.course.find({}, {'_id': False}))
+    return jsonify({'courses':course_list})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=8888, debug=True)
+    app.run('0.0.0.0', port=5353, debug=True)
